212d47515ccfe4fdcc674ab55d3f14b4908d10e3
yourusername <email@youremail.com>
2023-02-17 20:22:55 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "safeinput.h" 
+
+/*
+void charPosition & nroftimesInString(){
+    
+    char text[100];
+    printf("Write a text");
+    gets(text);
+    
+    int character;
+    printf("Name a character\n");
+    scanf("%c", &character);
+
+
+    char * find = strchr(text, character); // FIND NEEDLE IN HAYSTACK
+    if (find==NULL)
+        printf("Did not find the character %c in the text", character);
+    else{
+        printf("Found the character %c, it was: %d", character, find-text);
+    }
+    
+void FindnrcharinLoop(){
+    char text[100];
+    int charInput[2];
+    
+    printf("Write a text: \n");
+    gets(text);
+
+    printf("Write in a character: \n");
+    scanf("%c", &charInput);
+    
+    char character=charInput[0];
+
+    char *find = strchr(text, character);
+
+    int charcount= 0;
+    for (int i=0; i<=strlen(text); i++){
+        if (text[i] == character)// if i in text == character
+            charcount++;
+    } // NEXT IF & ELSE IF OUTSIDE FOR-LOOP, SO THE TEXT 
+    //ONLY GETS PRINTED ONCE AND NOT AS MANY TIMES AS CHARS IN TEXT.
+    if(charcount ==0)
+         printf("There was no such char\n");
+
+    else
+        printf("The character %c was found %d times.", character, charcount);
+    
+}*/
+
+void main(){
+
+
+}
+
