44ee01e12e0829de8e9e6b2d4db570eb2d155a0e
yourusername <email@youremail.com>
2023-02-18 21:24:07 +0100

    4

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ void charPosition & nroftimesInString(){
     if (find==NULL)
         printf("Did not find the character %c in the text", character);
     else{
-        printf("Found the character %c, it was: %d", character, find-text);
+        printf("Found the character %c, it was in position: %d", character, find-text);
     }
     
 void FindnrcharinLoop(){
@@ -50,10 +50,10 @@ void FindnrcharinLoop(){
     else
         printf("The character %c was found %d times.", character, charcount);
     
-}*/
+}
 
-void main(){
-    char string[] = "This is a string you are supposed to change";
+void changestringReplaceaChar(){
+    char string[] = "This is a string you are supposed to change"; //Förbestämd mening... byt ut ' '
 
     for(int i=0; i<strlen(string);i++){
         if(string[i]== ' ')
@@ -61,14 +61,39 @@ void main(){
     }
 
     int count = 0;
-    for(int i=0; i<strlen(string);i++){
+    for(int i=0; i<strlen(string);i++){ 
         if(string[i] =='*')
             count++;
     }
 
     printf("I found %c    %d times", '*', count);
-
-
-
 }
+Be användaren mata in en mailadress. Programmet skall kontrollera att inmatningen är rätt dvs att det finns
+ett @ tecken och att det finns en . med 2 eller 3 tecken efter. 
+Meddela användaren om det är rätt eller felaktig adress
+*/
+void main(){
+    char email[100];
+   
+    GetInput("Enter your email-address please", email, sizeof(email));
 
+
+    bool containsAt =strchr(email,'@'); // om ej @ i inmatning så händer inget i consolen.
+    bool containsDot = false; // sätt till falskt (innehåller varken.och är inte ok efter.) 
+    bool okAfterDot = false; //så man sen kan sätta premisser för sant.
+
+    char *lastDot= strchr(email, '.');
+
+    if(lastDot){
+        containsDot = true;
+        int pos = lastDot - email; // Då email innehåller . kollar vi VAR . är
+        int after = strlen(email)- pos - 1; // strl på allt som kommer efter.
+        okAfterDot = after ==2 || after ==3;
+    }
+
+    if(containsAt && containsDot && okAfterDot){
+        printf("Ok email");
+    }else
+        printf("invalid email");
+    
+}
