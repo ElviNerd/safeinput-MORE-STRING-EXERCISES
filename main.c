6bae53cdd7145b5b7c6c39f2c180d1674bae7654
yourusername <email@youremail.com>
2023-02-18 22:59:14 +0100

    hej

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include "safeinput.h" 
 
+
 /*
 void charPosition & nroftimesInString(){
     
@@ -71,12 +72,11 @@ void changestringReplaceaChar(){
 Be användaren mata in en mailadress. Programmet skall kontrollera att inmatningen är rätt dvs att det finns
 ett @ tecken och att det finns en . med 2 eller 3 tecken efter. 
 Meddela användaren om det är rätt eller felaktig adress
-*/
-void main(){
+
+void valideringAvMejl(){
     char email[100];
    
     GetInput("Enter your email-address please", email, sizeof(email));
-
 
     bool containsAt =strchr(email,'@'); // om ej @ i inmatning så händer inget i consolen.
     bool containsDot = false; // sätt till falskt (innehåller varken.och är inte ok efter.) 
@@ -96,4 +96,50 @@ void main(){
     }else
         printf("invalid email");
     
+}
+
+
+
+int wordCount()
+{
+    char str[80];
+    int i, w = 0;
+    printf("Enter a string: ");
+    scanf("%[^\n]",str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+       
+        if((str[i]!=' ' && str[i+1]==' ')||(str[i+1]=='\0' && str[i]!=' '))
+        {
+            w++;
+        }
+        
+    }
+
+    printf("The number of words = %d", w );
+
+    return 0;
+}*/
+int main(){
+    char words[300];
+    GetInput("Mata in ett ord eller en mening", words, sizeof(words));
+    // Start from leftmost & 
+    // rightmost corners of str
+    int l = 0;
+    int h = strlen(words) - 1; // -1 because of the /0
+  
+    // Keep comparing characters
+    // while they are same
+    while (h > l) {
+        if (words[l++] != words[h--]) {
+            printf("%s is not a palindrome\n", words);
+            return 0;
+            // will return from here
+        }
+    }
+  
+    printf("You found a palindrome %s \n", words);
+  
+    return 0;
 }
